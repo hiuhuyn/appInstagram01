dd679f4e9624298a68aa1df455a9d4def576c7bb
Nguyễn Minh Quân <83531095+izumi291122@users.noreply.github.com>
2023-01-26 23:09:06 +0700

    fix textView null

--- instagram01/app/src/main/java/com/example/instagram01/activity/LoginActivity.kt
+++ instagram01/app/src/main/java/com/example/instagram01/activity/LoginActivity.kt
@@ -11,20 +11,33 @@ class LoginActivity : AppCompatActivity() {
     private var check: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val sigup = findViewById<TextView>(R.id.tv_sigup)
+        // chưa khởi tạo layout
         val myperf: SharedPreferences = getSharedPreferences("dataSave", MODE_PRIVATE)
         check = myperf.getBoolean("checkLogin", false)
+        check = false
+
+        // check xem đẽ đăng nhập chưa
         if (check){
+            // đã đăng nhập thì next sang HomePage luôn ko tạo layout activity_login nữa
             val i = Intent(this, HomePageActivity::class.java)
             startActivity(i)
             finish()
         }else{
+            // chưa đăng nhập thì nhảy vào đây, bây giờ mới tạo layout
             setContentView(R.layout.activity_login)
-            check = true // kiểm tra login thành công thì đổi check = true
+            // viết nội dung trong này
+//            check = true // kiểm tra login thành công thì đổi check = true
+            val sigup = findViewById<TextView>(R.id.tv_sigup)
+
+            sigup.setOnClickListener {
+                startActivity(Intent(this, SigupActivity::class.java))
+            }
+
+
+
+
         }
-        sigup.setOnClickListener {
-            startActivity(Intent(this, SigupActivity::class.java))
-        }
+
     }
     override fun onPause() {
         super.onPause()
